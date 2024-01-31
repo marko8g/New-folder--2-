c0afd78b349fa17e0c49a4e71c69daafb2dd4f9d
marko8g <carrera.mark@yahoo.com>
2024-01-30 16:05:55 -0800

    project

---
+++ port.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger")
+hamburger.onclick = function() {
+    let navbar = document.getElementsByTagName("nav")[0]
+     navbar.classList.toggle("active")
+} 
+const scroll = document.querySelector("#scroll-buttons")
+onclick.scroll
