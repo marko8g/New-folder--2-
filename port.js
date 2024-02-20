31881c67c09e8a40b9634d95e9e91b446ef996c0
marko8g <carrera.mark@yahoo.com>
2024-02-19 16:13:37 -0800

    Fixed about me section

--- port.js
+++ port.js
@@ -1,7 +1,25 @@
-const hamburger = document.querySelector(".hamburger")
-hamburger.onclick = function() {
-    let navbar = document.getElementsByTagName("nav")[0]
-     navbar.classList.toggle("active")
-} 
-const scroll = document.querySelector("#scroll-buttons")
-onclick.scroll
+// const hamburger = document.querySelector(".hamburger")
+// hamburger.onclick = function() {
+//     let navbar = document.getElementsByTagName("nav")[0]
+//      navbar.classList.toggle("active")
+// } 
+// const scroll = document.querySelector("#scroll-buttons")
+// onclick.scroll
+
+
+let scrollContainer = document.querySelector(".middle-project-content");
+let next = document.querySelector("#scroll-buttons-1")
+let back = document.querySelector("#scroll-buttons-2")
+scrollContainer.addEventListener("wheel",(evt) => {
+    evt.preventDefault()
+    scrollContainer.scrollLeft+= evt.deltaY
+}) 
+next.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = "smooth"
+    scrollContainer.scrollLeft += 600
+})
+
+back.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = "smooth"
+    scrollContainer.scrollLeft -= 600
+})
