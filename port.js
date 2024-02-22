afb8f0e78ba6a6984b72309ecee46abe09713453
marko8g <carrera.mark@yahoo.com>
2024-02-22 03:52:22 -0800

    Added working contact section and fixed resposivness for all sections

--- port.js
+++ port.js
@@ -7,19 +7,24 @@
 // onclick.scroll
 
 
-// let scrollContainer = document.querySelector(".middle-project-content");
-// let next = document.querySelector("#scroll-buttons-1")
-// let back = document.querySelector("#scroll-buttons-2")
-// scrollContainer.addEventListener("wheel",(evt) => {
-//     evt.preventDefault()
-//     scrollContainer.scrollLeft+= evt.deltaY
-// }) 
-// next.addEventListener("click", () => {
-//     scrollContainer.style.scrollBehavior = "smooth"
-//     scrollContainer.scrollLeft += 600
-// })
 
-// back.addEventListener("click", () => {
-//     scrollContainer.style.scrollBehavior = "smooth"
-//     scrollContainer.scrollLeft -= 600
-// })
+const hamMenu = document.querySelector('.hamburger');
+// variables for hambuger and off-screen-menu so we can target them later   
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+});
+
+// Event listener for clicks outside the menu
+document.body.addEventListener('click', function(event) {
+    const isClickInsideMenu = offScreenMenu.contains(event.target);
+    const isClickOnHamburger = hamMenu.contains(event.target);
+    
+    if (!isClickInsideMenu && !isClickOnHamburger) {
+        // If the click is outside the menu and not on the hamburger, close the menu
+        hamMenu.classList.remove('active');
+        offScreenMenu.classList.remove('active');
+    }
+});
